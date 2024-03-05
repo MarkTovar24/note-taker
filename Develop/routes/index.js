7f82985c12c83701cdfac02d274de1078b60d8e8
Mark Tovar <markjtovar24@gmail.com>
2024-03-05 17:14:29 -0600

    added nessecary packages to run program and got the routes for homepage and notes

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,1 +1,17 @@
 const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+const uniqid = require('uniqid');
+const express = require('express');
+const app = express();
+
+//get route for homepage
+app.get('/', (req, res) =>
+   res.sendFile(path.join(__dirname, '/public/index.html'))
+ );
+
+ //get route for the notes page 
+ app.get("/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+ );
+
--- Develop/server.js
+++ Develop/server.js
@@ -3,26 +3,26 @@ const express = require('express');
 const PORT = 3001;
 const app = express();
 const api = require('./routes/index.js');
-const path = require('path');
+
+
 
 
 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-
 app.use(express.static('public'));
+app.use(api);
 
+// //get route for homepage
+// app.get('/', (req, res) =>
+//    res.sendFile(path.join(__dirname, '../public/index.html'))
+//  );
 
-//get route for homepage
-app.get('/', (req, res) =>
-   res.sendFile(path.join(__dirname, '/public/index.html'))
- );
-
- //get route for the notes page 
- app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
- );
+//  //get route for the notes page 
+//  app.get('/notes', (req, res) =>
+//     res.sendFile(path.join(__dirname, '/public/notes.html'))
+//  );
 
 
  app.listen(PORT, () =>
